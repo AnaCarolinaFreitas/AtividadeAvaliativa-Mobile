f9e4a683d48d528e5f435cd377a4db03827680f2
joaoporto27 <joaovitorportosales46>
2025-04-15 11:40:47 -0300

    Feat: primeiro commit

---
+++ pages/Joao01.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView, Image, View } from "react-native";
+import Bloco from "../components/Block"
+import Card from "../components/Card"
+
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+    <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+      <View style={styles.header}>
+        <View style={styles.left}>
+          <Image source={require("../assets/bolapreta.png")} style={styles.icon} ></Image>
+        </View>
+        <View style={styles.right}>
+          <Image source={require("../assets/bolabranca.jpg")} style={styles.icon}></Image>
+          <Image source={require("../assets/bolabranca.jpg")} style={styles.icon}></Image>
+        </View>
+      </View>
+      <View style={styles.bloco}>
+        <Bloco />
+        <Bloco />
+      </View>
+      <View style={styles.rolagem}>
+        <Card />
+        <Card />
+        <Card />
+      </View>
+    </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "white",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: "2rem",
+  },
+  left: {
+    backgroundColor: "white",
+    width: "10rem",
+    height: "10rem",
+    width: "10rem",
+  },
+  right: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  icon: {
+    height: "2rem",
+    width: "2rem",
+    margin: "1rem"
+  },
+  bloco: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  rolagem: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "1rem",
+    margin: "1rem"
+  }
+})
---
+++ pages/Joao02.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+export default function PerfilJoao() {
+    return (
+        <View style={style.container}>
+        <Image>
+
+        </Image>
+        <Text style={styles.title}> 
+            Descrição do meu perfil 
+        </Text>
+        
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {  
+    }
+})
