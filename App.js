6bd5ef650a9265b3f4af2fd6f09cf907121d1ca7
AnaCarolinaFreitas <ana.c.freitas35@aluno.senai.br>
2025-04-15 08:41:25 -0300

    update: App.js

--- App.js
+++ App.js
@@ -3,6 +3,18 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Ionicons";
 
+import Cadu01 from "./pages/Cadu01";
+import Cadu02 from "./pages/Cadu02";
+import Ana01 from "./pages/Ana01";
+import Ana02 from "./pages/Ana02";
+import Joao01 from "./pages/Joao01";
+import Joao02 from "./pages/Joao02";
+import Leo01 from "./pages/Leo01";
+import Leo02 from "./pages/Leo02";
+import Luiz01 from "./pages/Luiz01";
+import Luiz02 from "./pages/Luiz02";
+
+
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator () {
@@ -25,7 +37,7 @@ export default function TabNavigator () {
         )}} />
         <Tab.Screen name="Leo02" component={Leo02} options={{ tabBarIcon: ({ color, size}) => ( <Icon name="home" color={color} size={size} />
         )}} />
-        <Tab.Screen name="luiz01" component={luiz01} options={{ tabBarIcon: ({ color, size}) => ( <Icon name="home" color={color} size={size} />
+        <Tab.Screen name="Luiz01" component={Luiz01} options={{ tabBarIcon: ({ color, size}) => ( <Icon name="home" color={color} size={size} />
         )}} />
         <Tab.Screen name="Luiz02" component={Luiz02} options={{ tabBarIcon: ({ color, size}) => ( <Icon name="home" color={color} size={size} />
         )}} />
